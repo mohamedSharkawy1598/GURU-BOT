d98e98892414727aa76e8dd1a5acf2bfc93cee15
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2023-12-12 17:38:29 +0200

    Update econ-adddi.js

--- plugins/econ-adddi.js
+++ plugins/econ-adddi.js
@@ -4,27 +4,27 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw '✳️ tag the user'
+    if (!who) throw '*「قم بلاشارة علي شخص👨‍💻」*'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw '✳️ Enter the amount of *Diamonds* you want to add'
-    if (isNaN(txt)) throw '🔢 only numbers'
+    if (!txt) throw '*「أدخل كمية _الماس_ التي تريد إضافتها」*'
+    if (isNaN(txt)) throw '*「جميع الارقام」*'
     let dmt = parseInt(txt)
     let diamond = dmt
     
-    if (diamond < 1) throw '✳️ Mínimum  *1*'
+    if (diamond < 1) throw '*「الحد الأدنى 1」*'
     let users = global.db.data.users
    users[who].diamond += dmt
 
-    await m.reply(`≡ *💎 ADDED*
-┌──────────────
-▢ *Total:* ${dmt}
-└──────────────`)
+    await m.reply(`*「تم الاضافة」*
+*┌●━──━𓊆اضافة الماس𓊇━──━●*
+*╎𖣐➽ المجموع:* ${dmt}
+*└●━──𓊆⍣⃝𝑺𝑈𝑁𝐺𓊇──━●*`)
    conn.fakeReply(m.chat, `▢ Did you receive \n\n *+${dmt}* Diamonds`, who, m.text)
 }
 
 handler.help = ['adddi <@user>']
 handler.tags = ['econ']
-handler.command = ['adddi'] 
+handler.command = ['اضافة-الماس'] 
 handler.rowner = true
 
 export default handler
